44f33280c9d73527b5d1104944501841dfe0b5f8
etienne <etiennerodriguez3@gmail.com>
2022-07-17 14:21:28 -0400

    for now, correctly takes in data and displays a single engineers information, however role is undefined

--- index.js
+++ index.js
@@ -2,17 +2,16 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// const generateMarkdown = require('./src/generateMarkdown.js');
+const generateMarkdown = require('./src/generateMarkdown.js');
 
 //importing all the files that are needed
-const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
 /* Starting the questions, in order to do this properly I will need to
 store the data correctly based on a persons role */
-const promptUserEmployee = () => {
+const promptUserEngineer = () => {
  return inquirer.prompt([
     {
         type:'input',
@@ -51,12 +50,41 @@ const promptUserEmployee = () => {
         }
     },
     {
-        type: 'confirm',
-        name:'confirmAddEmployee',
-        message: 'Would you like to add another employee?',
-        default: false
-    }
- ])
+        type: 'input',
+        name: 'github',
+        message: "Please enter the employee's github username.",
+        validate: githubInput => {
+            if(githubInput) {
+                return true;
+            } else {
+               console.log('Invalid github username!');      
+            }
+        }
+    },
+    //eventually add option to add another engineer?
+ ]);
+};
+
+//writing a new html file
+const writeToFile = fileContent => {
+    return new Promise ((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve ({
+                ok: true,
+                message: 'The HTML page has been generated! Open to see result.'
+            });
+        });
+    });
 }
 
-promptUserEmployee()
+promptUserEngineer()
+.then(nameInput => {
+    return generateMarkdown(nameInput)
+})
+.then(pageHTML => {
+    return writeToFile(pageHTML)
+})
--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,1 +1,46 @@
+function generateMarkdown(data) {
+    return `
+    
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./style.css">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <header>My Team</header>
+        
+        <article class="team">
+            <div class="employees">
+                <h1>${data.name}</h1>
+                <h2>${data.role}</h2>
+                <div class="employee-info">
+                    <table>
+                        <tr>
+                            <td>Id:</td>
+                            <td>${data.id}</td>
+                        </tr>
+                        <tr>
+                            <td>Email:</td>
+                            <td>${data.email}</td>
+                        </tr>
+                        <tr>
+                            <td>Github:</td>
+                            <td><a href='https://github.com/${data.github}'>${data.github}</a></td>
+                        </tr>
+                        
+                    </table>
+                </div>
+            </div>
+        </article>
+    </body>
+    </html>
+    
+    `
+}
+
+
 module.exports = generateMarkdown;
